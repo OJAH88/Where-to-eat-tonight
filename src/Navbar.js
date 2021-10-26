615df9a66b310bbed093c0bf9f7b0a4cfbac359e
Jane <janechodance@gmail.com>
2021-10-26 11:04:22 -0400

    nav

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,17 +2,23 @@ import { Link } from "react-router-dom";
 
 function Navbar(){
     return (
-        <nav>
-        <h1>Where To Eat!</h1>
-        <div className="links">
-            <Link to="/">Home</Link> 
-            <Link to="/jane">Jane's Pick</Link> 
-            <Link to="/jesse">Jesse's Pick</Link> 
-            <Link>Add A Restaurant</Link>
-
-
-        </div>
-        </nav>
+        <nav className="navbar">
+            <h1>Where To Eat</h1>
+            <div className="links">
+                <Link to="/">Home</Link> 
+                <Link to="/jane" style={{
+                    color: 'white',
+                    backgroundColor: '#f1356d',
+                    borderRadius: '8px'
+                }}>Jane's Picks</Link>
+                  <Link to="/jesse" style={{
+                    color: 'white',
+                    backgroundColor: '#f1356d',
+                    borderRadius: '8px'
+                }}>Jesse's Picks</Link>
+               
+            </div>
+            </nav>
     );
 }
  
